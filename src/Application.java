4e548398156f373f6df2d9e706d7d7b2faac0b9f
Mathieu <mathieu.belanger.2@ens.etsmtl.ca>
2015-10-02 10:51:39 -0400

    initial commit

---
+++ src/Application.java
@@ -0,0 +1,10 @@
+public class Application {
+
+	/**
+	 * Launch the application
+	 * @param args This parameter is ignored
+	 */
+	public static void main(String args[]) {
+		System.out.println("Audio Resample project!");
+	}
+}
